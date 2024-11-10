c5dfc9844e94770fbd6e4f805d2853e8ef178469
emerson <xinayespinoza@gmail.com>
2024-11-10 14:38:36 -0300

    fix app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Proyecto Front con react y Back con Flask
+Pasos para producción de frontend, desde la carpeta de frontend
+```bash
+npm run build
+```
--- backend_compi/app.py
+++ backend_compi/app.py
@@ -1,18 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
+import os
 
-app = Flask(__name__)
-CORS(app)  # Permite conexiones de origen cruzado
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+
+# Habilitar CORS para permitir solicitudes desde React (si es necesario)
+CORS(app)
+
+# Ruta de la API
 
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    # Datos de ejemplo que se enviarán al frontend
     data = {
         "message": "¡Hola desde Flask!",
         "items": ["item1", "item2", "item3"]
     }
     return jsonify(data)
+
+# Ruta para servir los archivos estáticos de React (index.html)
+
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# Ruta para servir cualquier archivo estático de React (imágenes, JS, CSS, etc.)
+
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
 
 
 if __name__ == '__main__':
